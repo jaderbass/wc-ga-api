9a834b23c3fbec61a1b52cf82117d39b3b100f7b
Jörg Aderhold <joerg@jaderbass.de>
2025-06-25 21:15:16 +0200

    change ImporterForSingingRock add fixedValues()-method

--- app/Imports/Manufacturer/ImporterForSingingrock.php
+++ app/Imports/Manufacturer/ImporterForSingingrock.php
@@ -4,28 +4,35 @@ namespace App\Imports\Manufacturer;
 
 use App\Imports\BaseXmlImporter;
 use App\Models\Product;
+use App\Helpers\XmlValueSanitizer;
 
-class ImporterForKask extends BaseXmlImporter
+class ImporterForSingingRock extends BaseXmlImporter
 {
     protected function model(): string
     {
         return Product::class;
     }
 
+    protected function fixedValues(): array
+    {
+        return [
+            'manufacturer_id' => 5,
+        ];
+    }
+
     protected function mapXmlItem(\SimpleXMLElement $item): array
     {
         return [
-            'productnumber'       => (string) $item->Code,
-            'productname'         => (string) $item->ProductName,
-            'description'         => (string) $item->Description,
-            'eancode'             => (string) $item->EAN,
-            'price'               => \App\Helpers\XmlValueSanitizer::toIntCents($item->Price),
-            'width'               => \App\Helpers\XmlValueSanitizer::toScaledInt($item->Width, 10),
-            'height'              => \App\Helpers\XmlValueSanitizer::toScaledInt($item->Height, 10),
-            'weight'              => \App\Helpers\XmlValueSanitizer::toScaledInt($item->Weight, 1000),
-            'active'              => ((string) $item->Status) === 'active',
-            'variant_group'       => (string) $item->VariantGroup,
-            'manufacturer_id'     => 5, // Beispiel-ID für Singing Rock
+            'productnumber'   => (string) $item->Code,
+            'productname'     => (string) $item->ProductName,
+            'description'     => (string) $item->Description,
+            'eancode'         => (string) $item->EAN,
+            'price'           => XmlValueSanitizer::toIntCents($item->Price),
+            'width'           => XmlValueSanitizer::toScaledInt($item->Width, 10),
+            'height'          => XmlValueSanitizer::toScaledInt($item->Height, 10),
+            'weight'          => XmlValueSanitizer::toScaledInt($item->Weight, 1000),
+            'active'          => ((string) $item->Status) === 'active',
+            'variant_group'   => (string) $item->VariantGroup,
         ];
     }
 }
