4b6ab5f8495d41c39f159d8fe84896e5b348057e
Jörg Aderhold <joerg@jaderbass.de>
2025-07-25 11:57:11 +0200

    customize dashboard

--- resources/views/filament/widgets/manufacturer-count-widget.blade.php
+++ resources/views/filament/widgets/manufacturer-count-widget.blade.php
@@ -1,5 +1,9 @@
 <x-filament-widgets::widget>
   <x-filament::section>
+        {{-- Icon --}}
+        <x-heroicon-o-building-storefront class="h-10 w-10 text-white" />
+
+        {{-- Text & Zahl --}}
     <h3 class="text-lg font-medium text-gray-100">Hersteller gesamt</h3>
     <p class="text-4xl font-bold text-white">{{ $count }}</p>
   </x-filament::section>
--- resources/views/filament/widgets/product-count-widget.blade.php
+++ resources/views/filament/widgets/product-count-widget.blade.php
@@ -1,6 +1,12 @@
 <x-filament-widgets::widget>
   <x-filament::section>
-    <h3 class="text-lg font-medium text-gray-100">Produkte gesamt</h3>
-    <p class="text-4xl font-bold text-white">{{ $count }}</p>
+    {{-- Icon --}}
+    <x-heroicon-o-archive-box class="h-10 w-10 text-white" />
+
+    {{-- Text & Zahl --}}
+    <div>
+      <h3 class="text-lg font-medium text-gray-100">Produkte gesamt</h3>
+      <p class="text-4xl font-bold text-white">{{ $count }}</p>
+    </div>
   </x-filament::section>
 </x-filament-widgets::widget>
