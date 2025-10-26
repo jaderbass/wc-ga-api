67e1d81be50023b4658397fd8ac36033786f950d
Jörg Aderhold <joerg@jaderbass.de>
2025-10-26 19:22:20 +0100

    fix(woo): correct JsonResponse import and typehint in WooWebhookController

--- app/Http/Controllers/WooWebhookController.php
+++ app/Http/Controllers/WooWebhookController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use App\Models\ProductVariation;
 use App\Models\Shop;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
 
 class WooWebhookController extends Controller
