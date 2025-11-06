c87e391e37f282c71024d2f98818731e0ff7b740
Jörg Aderhold <joerg@example.com>
2025-11-06 14:31:22 +0100

    fix(app): simplify root redirect to /admin or /admin/login

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+/*
 Route::get('/', function () {
     return Auth::check()
         ? redirect()->route('filament.admin.pages.dashboard')
         : redirect()->route('filament.admin.auth.login');
 });
+*/
+
+Route::get('/', function () {
+    return Auth::check() ? redirect('/admin') : redirect('/admin/login');
+})->name('home');
 
 if (app()->environment('local')) {
     Route::view('/dev/select-style-test', 'dev.select-style-test')->name('dev.select-style-test');
