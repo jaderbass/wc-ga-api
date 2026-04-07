a40a4abb6de4aed30c40e422a3b2a36c10ae69da
Jörg Aderhold <joerg@example.com>
2026-04-07 18:59:08 +0200

    chore: remove unused relation manager import in edit product page

--- app/Filament/Resources/ProductResource/Pages/EditProduct.php
+++ app/Filament/Resources/ProductResource/Pages/EditProduct.php
@@ -3,7 +3,6 @@
 namespace App\Filament\Resources\ProductResource\Pages;
 
 use App\Filament\Resources\ProductResource;
-use App\Filament\Resources\ProductResource\RelationManagers\ProductVariantRelationManager;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
 
