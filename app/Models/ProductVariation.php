1a7534a1cfe2c30e6ddb103e550733d3aa58161f
Jörg Aderhold <joerg@example.com>
2026-09-17 12:03:50 +0200

    fix: enable product variation factory

--- app/Models/ProductVariation.php
+++ app/Models/ProductVariation.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductVariation extends Model
 {
+  use HasFactory;
+  
   protected $fillable = [
     'product_id',
     'woo_variation_id',
